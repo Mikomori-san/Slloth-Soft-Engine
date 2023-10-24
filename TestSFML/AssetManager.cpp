116b6cd781e30f09be19288224a437804329a865
Miko-senpai <43880346+Miko-senpai@users.noreply.github.com>
2023-10-24 20:17:52 +0200

    commit after pull errors

---
+++ TestSFML/AssetManager.cpp
@@ -0,0 +1,6 @@
+#include "AssetManager.h"
+
+AssetManager::loadTexture(char* name, char* fileName)
+{
+
+}
--- TestSFML/AssetManager.h
+++ TestSFML/AssetManager.h
@@ -11,6 +11,11 @@ public:
 	std::map<char*, sf::Music> Music = std::map<char*, sf::Music>{};
 	std::map<char*, sf::Font> Fonts = std::map<char*, sf::Font>{};
 
+	void loadTexture(char* name, char* fileName);
+	void loadSound(char* name, char* fileName);
+	void loadMusic(char* name, char* fileName);
+	void loadFont(char* name, char* fileName);
+
 private:
 	AssetManager() = delete;
 };
